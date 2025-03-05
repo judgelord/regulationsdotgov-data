24f3939b2496a822f51fc5208a0317443de3c5f4
judgelord <devin.jl@gmail.com>
2025-03-05 14:36:07 -0500

    #2 now working, skips files over 100MB for now

--- code/get_files.R
+++ code/get_files.R
@@ -1,72 +1,143 @@
+# load commonly used packages
 source("setup.R")
 
-#
+# the comments we already have metadata for
+doc_list <- list.files(path = here::here("data", "metadata"), pattern = "_comment_details.rda", recursive = T, include.dirs = T)
 
-# the agencies we already have
-doc_list <- list.dirs("data/metadata")
-
+# a data frame of parts of the document path
 docs <- tibble(
-  directory = doc_list,
-  agency = str_remove(doc_list, ".*metadata/") |> str_remove("/.*"),
-  docket = str_remove(doc_list, ".*metadata/") |> str_remove("/.+?"),
-  document = str_remove(doc_list, ".*/")
-)
+  metadata_path = paste0("data/metadata/", doc_list),
+  file_dir = paste0("data/files/", doc_list) |>  str_extract(".*/") |> str_remove("/$"),
+  agency = str_remove(doc_list, "/.*"),
+  docket = str_remove(doc_list, ".*?/") |> str_remove("/.*"),
+  document = str_remove(doc_list, ".*/") |> str_remove("_.*"),
+  details = str_remove(doc_list, ".*/")
+) |>
+  # drop aggregated details
+  filter(docket != document)
 
-docs <- docs |>  filter(str_detect(docket, "/")) |>
-  mutate(docket = str_remove(docket, "/.*") )
+head(docs) #|> transmute(docket == document)
 
-example_path <- here::here(
-  "data",
-  "meatadata",
-  #docs$document[1],
-  #paste0(docs$document[1], "_comment_details.rda")
-  "BLM",
-  "BLM-2015-0002",
-  "BLM-2015-0002-0016",
-  "BLM-2015-0002-0016_comment_details.rda"
-)
+dir.create(here::here("data", "files", docs$agency[1]))
 
-example_path <- here::here("data/metadata/BLM/BLM-2015-0002/BLM-2015-0002-0016/BLM-2015-0002-0016_comment_details.rda")
-
-load(example_path)
-
-
-
-###############################
-# NOT WELL DEVELOPED #
-# DOWNLOAD ATTACHMENTS
-
-#FIXME THIS DOES NOT YET WORK
-# extract attachments from details
-attachments <- comment_details$attachments |>
-  flatten() |>
-  map(as.data.frame) |> #FIXME, can we do this in one line?
-  map_dfr(~.x)
-
-# inspect
-ggplot(attachments) +
-  aes(x = size, fill = format) +
-  geom_bar()
-
-ggplot(attachments |> slice_max(size, n = 100)) +
-  aes(x = size, fill = format) +
-  geom_bar()
-
-#TODO? drop super large files?
-# can we store a record of files lost?
-# attachments %<>% filter(size < 100000000) # = < 100 MB ?
-
-attachment_urls <- attachments |>
-  pull(fileUrl) |>
-  unique()
-
-
-download_comments <- function(url){
-  #TODO
+for(i in docs$agency |> unique() ) {
+  dir.create(here::here("data", "files", i))
 }
 
+create_docket_folder <- function(agency, docket){
+  dir.create(here::here("data", "files", agency, docket))
+}
 
-download_comments(attachment_urls )
+walk2(docs$agency, docs$docket, create_docket_folder)
+
+create_doc_folder <- function(agency, docket, document){
+  dir.create(here::here("data", "files", agency, docket, document))
+}
+
+pwalk(list(docs$agency, docs$docket, docs$document), .f = create_doc_folder)
+############# END CREATE DIRECTORIES ###################
+
+# for testing
+agency <- docs$agency[8]
+docket <- docs$docket[8]
+document <- docs$document[8]
+metadata_path <- docs$metadata_path[8]
+
+# HELPERS
+# file hierarchy to determine which files are downloaded
+file_hierarchy <- tibble(
+  format = c("rtf", "htm", "txt",
+             "doc", "docx", "ppt", "pptx", "xlsx", "xls",
+             "pdf",
+             "jpg", "jpeg", "bmp", "tif", "png"),
+  priority = c(5, 5, 5,
+               4, 4, 4, 4, 4, 4,
+               2,
+               1, 1, 1, 1, 1)
+)
+
+### MAIN FUNCTION
+# FIXME this does not yet prioritize document types or filter out large files -- it downloads all attachments
+download_attachments <- function(agency, docket, document, metadata_path){
+  load(metadata_path)
+
+  attachments <- comment_details$attachments |>
+    # replace NULL with NA (WHY DO WE HAVE NULLS IN THESE DATA? -- A Q FOR THE PACKAGE )
+    map( ~ifelse(is.null(.x), NA, .x)) |>
+    # drop NA (WHY DO WE HAVE NAs IN THESE DATA? -- A Q FOR THE PACKAGE )
+    purrr::discard(is.na) |>
+    bind_rows() |>
+    unnest(cols = c(fileUrl, format, size))
+
+  file_types <- unique(attachments$format)
+  new_file_types <- file_types[which(!file_types %in% file_hierarchy$format)]
+
+  if( length(new_file_types) >0 ){
+    warning("File types not in file_hierarcy: ", new_file_types )
+  }
+
+  to_download <- attachments |>
+    # subset to comments with attachments
+    drop_na(fileUrl) |>
+    # files less than 100 MB
+    filter(size < 100000000) |>
+    # make id and number
+    mutate(number = fileUrl |> str_extract("_[0-9]*") |> str_remove("_"),
+           id = fileUrl |> str_remove(".*gov/") |> str_remove("/attachment.*"),
+           # make path
+           path = here::here("data", "files", agency, docket, document,
+                             paste0(id, "_", number, ".", format))) |>
+    group_by(id, number) |>
+    left_join(file_hierarchy, by = "format")  |>
+    # all unlisted file types get priority 0
+    mutate(priority = replace_na(priority, 0)) |>
+    slice_max(order_by = priority, n = 1)
+
+
+  # For each file url, download to path
+  for(i in 1:length(to_download$fileUrl) ){
+
+    if(!file.exists(to_download$path[i])){
+      tryCatch({
+
+    download.file(to_download$fileUrl[i],
+                  destfile = to_download$path[i])
+
+      }, error = function(e) {
+        message(e)
+      })
+
+    } else {
+      message( str_remove(to_download$fileUrl[i], ".*gov/"), " already downloaded")
+      }
+  }
+
+  }
+
+start = 0
+stop = 700
+# make a list to map over
+metadata_list <- list(agency = docs$agency[start:stop],
+                      docket = docs$docket[start:stop],
+                      document = docs$document[start:stop],
+                      metadata_path = docs$metadata_path[start:stop])
+
+# WARNING THIS DOWNLOADS ALL FILES
+pwalk(metadata_list, possibly(download_attachments, otherwise = "Fail"))
+
+# show file types missing from hierarchy
+warnings()
+
+# FOR INSPECTING
+load(metadata_list$metadata_path[51])
+
+# FAILS
+#33 - bulk docket needs converting
+# 46 - 0 attachmetns
+# 51 - 0 attachments
+
+# END
+####################################################################
 
 
 
